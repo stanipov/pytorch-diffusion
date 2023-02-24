8067fef8b32d347b005f1a9f0f3fddf2a212407c
SF <stanislav.filippov@gmail.com>
2023-02-23 22:53:53 -0500

    Added normalization and rescaling to the perceptual loss

--- src/models/perceptual_loss.py
+++ src/models/perceptual_loss.py
@@ -5,20 +5,32 @@ import torch.nn.functional as F
 from torchvision.models import vgg16, VGG16_Weights
 
 class PerceptualLoss(nn.Module):
-    def __init__(self, device = 'cuda', fp16 = torch.float16):
+    def __init__(self,  fp16 = torch.float16):
         super().__init__()
         vgg_w = VGG16_Weights.IMAGENET1K_V1
-        self.vgg16 = vgg16(weights=vgg_w).features
+        self.vgg16 = vgg16(weights=vgg_w).features.eval()
         for p in self.vgg16.parameters():
             p.requires_grad = False
-        self.vgg16.to(device)
-        
-        self.device = device
+
         self.fp16 = fp16
+        self.transform = torch.nn.functional.interpolate
+        self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
+        self.register_buffer("std", torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
         
-    def forward(self, x, x_recon, loss_type = 'huber'):
+    def forward(self, x, target, loss_type = 'huber'):
         
-        x_x = torch.cat((x, x_recon), dim = 0).to(self.device)
+        if x.shape[1] != 3:
+            x = x.repeat(1, 3, 1, 1)
+            target = target.repeat(1, 3, 1, 1)
+        
+        x = (x-self.mean) / self.std
+        target = (target-self.mean) / self.std
+        
+        if max(x.shape[-1], x.shape[-2]) > 224:
+            x = self.transform(x, mode='bilinear', size=(224, 224), align_corners=False)
+            target = self.transform(target, mode='bilinear', size=(224, 224), align_corners=False)
+                
+        x_x = torch.cat((x, target), dim = 0)
         sep = x.shape[0]
         
         if self.fp16:        

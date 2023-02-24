c108ce828010fcfd2c3f0a49ffc818b8f364b836
SF <stanislav.filippov@gmail.com>
2023-02-23 19:59:07 -0500

    added classes to the model

--- src/models/unet.py
+++ src/models/unet.py
@@ -10,13 +10,15 @@ from src.models.attention import Attention, LinearAttention
 class Unet(nn.Module):
     def __init__(
         self,
-        dim,
-        init_dim=None,
-        out_dim=None,
-        dim_mults=(1, 2, 4, 8),
-        channels=3,
-        self_condition=False,
-        resnet_block_groups=4,
+        img_size,
+        init_dim = None,
+        out_dim = None,
+        dim_mults = (1, 2, 4, 8),
+        time_dim = None,
+        channels = 3,
+        self_condition = False,
+        resnet_grnorm_groups = 4,
+        num_classes = None
     ):
         super().__init__()
 
@@ -25,20 +27,21 @@ class Unet(nn.Module):
         self.self_condition = self_condition
         input_channels = channels * (2 if self_condition else 1)
 
-        init_dim = default(init_dim, dim)
-        self.init_conv = nn.Conv2d(input_channels, init_dim, 1, padding=0) # changed to 1 and 0 from 7,3
+        init_dim = default(init_dim, img_size)
+        self.init_conv = nn.Conv2d(input_channels, init_dim, 1, padding = 0) # changed to 1 and 0 from 7,3
 
         dims = [init_dim, *map(lambda m: init_dim * m, dim_mults)] #dim*m
         in_out = list(zip(dims[:-1], dims[1:]))
         
-        block_klass = partial(ResnetBlock, groups=resnet_block_groups)
+        conv_unit = partial(ResnetBlock, groups=resnet_grnorm_groups)
 
         # time embeddings
-        time_dim = dim * 4
+        if not time_dim:
+            time_dim = img_size * 4
 
         self.time_mlp = nn.Sequential(
-            SinusoidalPositionEmbeddings(dim),
-            nn.Linear(dim, time_dim),
+            SinusoidalPositionEmbeddings(img_size),
+            nn.Linear(img_size, time_dim),
             nn.GELU(),
             nn.Linear(time_dim, time_dim),
         )
@@ -54,8 +57,8 @@ class Unet(nn.Module):
             self.downs.append(
                 nn.ModuleList(
                     [
-                        block_klass(dim_in, dim_in, time_emb_dim=time_dim),
-                        block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                        conv_unit(dim_in, dim_in, time_emb_dim=time_dim),
+                        conv_unit(dim_in, dim_in, time_emb_dim=time_dim),
                         Residual(PreNorm(dim_in, LinearAttention(dim_in))),
                         Downsample(dim_in, dim_out)
                         if not is_last
@@ -65,9 +68,9 @@ class Unet(nn.Module):
             )
 
         mid_dim = dims[-1]
-        self.mid_block1 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+        self.mid_block1 = conv_unit(mid_dim, mid_dim, time_emb_dim=time_dim)
         self.mid_attn = Residual(PreNorm(mid_dim, Attention(mid_dim)))
-        self.mid_block2 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+        self.mid_block2 = conv_unit(mid_dim, mid_dim, time_emb_dim=time_dim)
 
         for ind, (dim_in, dim_out) in enumerate(reversed(in_out)):
             is_last = ind == (len(in_out) - 1)
@@ -75,8 +78,8 @@ class Unet(nn.Module):
             self.ups.append(
                 nn.ModuleList(
                     [
-                        block_klass(dim_out + dim_in, dim_out, time_emb_dim=time_dim),
-                        block_klass(dim_out + dim_in, dim_out, time_emb_dim=time_dim),
+                        conv_unit(dim_out + dim_in, dim_out, time_emb_dim=time_dim),
+                        conv_unit(dim_out + dim_in, dim_out, time_emb_dim=time_dim),
                         Residual(PreNorm(dim_out, LinearAttention(dim_out))),
                         Upsample(dim_out, dim_in)
                         if not is_last
@@ -87,10 +90,14 @@ class Unet(nn.Module):
 
         self.out_dim = default(out_dim, channels)
 
-        self.final_res_block = block_klass(dim_in*2, dim, time_emb_dim=time_dim) # dim * 2
+        self.final_res_block = conv_unit(dim_in*2, dim, time_emb_dim=time_dim) # dim * 2
         self.final_conv = nn.Conv2d(dim, self.out_dim, 1)
+        
+        if num_classes:
+            self.num_classes = num_classes
+            self.lbl_embeds = nn.Embedding(num_classes,time_dim)
 
-    def forward(self, x, time, x_self_cond=None):
+    def forward(self, x, time, x_self_cond=None, lbls = None):
         if self.self_condition:
             x_self_cond = default(x_self_cond, lambda: torch.zeros_like(x))
             x = torch.cat((x_self_cond, x), dim=1)
@@ -99,6 +106,9 @@ class Unet(nn.Module):
         r = x.clone()
 
         t = self.time_mlp(time)
+        
+        if lbls is not None:
+            t += self.lbl_embeds(lbls)
 
         h = []
 

6bf9790225fae0fa08d9bb8a9bdee559859cb2a3
SF <stanislav.filippov@gmail.com>
2023-04-16 18:14:17 -0400

    Added a function to set the DataLoader from a training config

--- src/datasets/artbench.py
+++ src/datasets/artbench.py
@@ -1,8 +1,16 @@
+import numpy as np
+import torch
+
 from PIL import Image
 import numpy as np
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
-
+from torch.utils.data import random_split
+# ==================================================================================================
+#
+#                           Datasets
+#
+# ==================================================================================================
 def artbench256(root):
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Lambda(lambda t: (t * 2) - 1)])
@@ -11,7 +19,6 @@ def artbench256(root):
     
 def artbench_hires(root, image_size = False):
     if image_size:
-        #int(np.ceil(image_size*1.25)
         transform = transforms.Compose([transforms.Resize(image_size,
                                         interpolation=transforms.InterpolationMode.BICUBIC,
                                         antialias = True),
@@ -23,3 +30,43 @@ def artbench_hires(root, image_size = False):
                                     transforms.Lambda(lambda t: (t * 2) - 1)])
     
     return ImageFolder(root = root, loader=Image.open, transform = transform)
+# ==================================================================================================
+#
+#                           Loaders
+#
+# ==================================================================================================
+def set_dataloader_vq(config):
+    # Dataset params
+    image_size         = config['dataset']['image_size']
+    root               = config['dataset']['location']
+    use_subset         = config['dataset']['use_subset']
+    batch_size         = int(config['training']['batch_size'])
+    dataloader_workers = int(config['training']['dataloader_workers'])
+    if use_subset:
+        use_subset = float(use_subset)
+    img_resize = config['dataset']['img_resize']
+    
+    print('Setting the dataset')
+    if img_resize and image_size > 256:
+        print(f'Using the original dataset with rescaling to {image_size} pix')
+        dataset = artbench_hires(root, image_size=image_size)
+    else:
+        print('Using CIFAR10-like dataset of 256 pix')
+        dataset = artbench256(root)
+    num_classes = len(dataset.classes)
+
+    if use_subset:
+        print(f'\t{use_subset} of the total dataset will be used')
+        new_ds_len = int(len(dataset)*use_subset)
+        splits = [new_ds_len, len(dataset) - new_ds_len]
+        dataset, _ = random_split(dataset, splits) 
+        print(f'\t{len(dataset)} of images will be used')
+    else:
+        print(f'Using whole of {len(dataset)} images')
+    print(f'\t{num_classes} classes were found')
+
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                              shuffle=True, num_workers=dataloader_workers,
+                                              pin_memory = True)
+    print('Done')
+    return train_loader

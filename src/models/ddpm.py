f1f8022891dd08d8b964763a69f07e6e8ece315c
SF <stanislav.filippov@gmail.com>
2023-05-29 19:01:38 -0400

    (1) Modified DDPM, (2) Added selection of noise schedulers

--- src/models/ddpm.py
+++ src/models/ddpm.py
@@ -1,18 +1,126 @@
 import torch, time
+import torch.nn as nn
 import torch.nn.functional as F
 from tqdm import tqdm
 from typing import Dict, Tuple
+from src.models.noise_schedulers import cosine_beta_schedule, linear_beta_schedule, quadratic_beta_schedule, sigmoid_beta_schedule
+from src.losses.util import loss_fn
 
-# ------------------------------------------ Newer version --------------------------------------
-# Taken from https://github.com/cloneofsimo/minDiffusion/blob/master/mindiffusion/ddpm.py
-
-
-
+# ------------------------------------------ Newer version ------------------------------------------
 
 
+def get_betas(timesteps: int, noise_kw: Dict):
+    """
+    Computes betas according to the scheduler type and required parameters.
+    The function has default parameters.
+    SF
+    """
+    scheduler_name = noise_kw['type']
+    if 'cosine' in  scheduler_name:
+        scheduler = cosine_beta_schedule
+        s = noise_kw.get('s', 0.008)
+        betas = scheduler(timesteps, s)
+    elif 'linear' in scheduler_name:
+        scheduler = linear_beta_schedule
+        beta_start = noise_kw.get('beta_start', 0.0001)
+        beta_end = noise_kw.get('beta_end', 0.02)
+        betas = scheduler(timesteps, beta_start, beta_end)
+    elif 'quadratic' in scheduler_name:
+        scheduler = quadratic_beta_schedule
+        beta_start = noise_kw.get('beta_start', 0.0001)
+        beta_end = noise_kw.get('beta_end', 0.02)
+        betas = scheduler(timesteps, beta_start, beta_end)
+    elif 'sigmoid' in scheduler_name:
+        scheduler = sigmoid_beta_schedule
+        beta_start = noise_kw.get('beta_start', 0.0001)
+        beta_end = noise_kw.get('beta_end', 0.02)
+        betas = scheduler(timesteps, beta_start, beta_end)
+    else:
+        raise ValueError(f'Scheduler type of "{scheduler_name}" is not recognized')
+    return betas
 
+def ddpm_schedules(beta_t: float, T: int) -> Dict[str, torch.Tensor]:
+    """
+    Returns pre-computed schedules for DDPM sampling, training process.
+    Adopted from https://github.com/cloneofsimo/minDiffusion/blob/master/mindiffusion/ddpm.py
+    """
+    sqrt_beta_t = torch.sqrt(beta_t)
+    alpha_t = 1 - beta_t
+    log_alpha_t = torch.log(alpha_t)
+    alphabar_t = torch.cumsum(log_alpha_t, dim=0).exp()
 
-#  -------------------------------------------------------
+    sqrtab = torch.sqrt(alphabar_t)
+    oneover_sqrta = 1 / torch.sqrt(alpha_t)
+
+    sqrtmab = torch.sqrt(1 - alphabar_t)
+    mab_over_sqrtmab_inv = (1 - alpha_t) / sqrtmab
+
+    return {
+        "alpha_t": alpha_t,  # \alpha_t
+        "oneover_sqrta": oneover_sqrta,  # 1/\sqrt{\alpha_t}
+        "sqrt_beta_t": sqrt_beta_t,  # \sqrt{\beta_t}
+        "alphabar_t": alphabar_t,  # \bar{\alpha_t}
+        "sqrtab": sqrtab,  # \sqrt{\bar{\alpha_t}}
+        "sqrtmab": sqrtmab,  # \sqrt{1-\bar{\alpha_t}}
+        "mab_over_sqrtmab": mab_over_sqrtmab_inv,  # (1-\alpha_t)/\sqrt{1-\bar{\alpha_t}}
+    }
+
+class DDPM(nn.Module):
+    def __init__(
+        self,
+        eps_model: nn.Module,
+        noise_kw: Dict,
+        n_T: int,
+        loss_func: str = 'huber'
+    ) -> None:
+        # Adopted from https://github.com/cloneofsimo/minDiffusion/blob/master/mindiffusion/ddpm.py
+        super().__init__()
+        self.eps_model = eps_model
+
+        betas = get_betas(n_T, noise_kw)
+
+        # register_buffer allows us to freely access these tensors by name. It helps device placement.
+        for k, v in ddpm_schedules(betas, n_T).items():
+            self.register_buffer(k, v)
+
+        self.n_T = n_T
+        self.criterion = loss_fn(loss_func)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Makes forward diffusion x_t, and tries to guess epsilon value from x_t using eps_model.
+        This implements Algorithm 1 in the paper.
+        """
+        _ts = torch.randint(1, self.n_T + 1, (x.shape[0],)).to(x.device)
+        # t ~ Uniform(0, n_T)
+        eps = torch.randn_like(x)  # eps ~ N(0, 1)
+
+        x_t = (
+            self.sqrtab[_ts, None, None, None] * x
+            + self.sqrtmab[_ts, None, None, None] * eps
+        )  # This is the x_t, which is sqrt(alphabar) x_0 + sqrt(1-alphabar) * eps
+        # We should predict the "error term" from this x_t. Loss is what we return.
+
+        return self.criterion(eps, self.eps_model(x_t, _ts / self.n_T))
+
+    def sample(self, n_sample: int, size, device) -> torch.Tensor:
+
+        x_i = torch.randn(n_sample, *size).to(device)  # x_T ~ N(0, 1)
+
+        # This samples accordingly to Algorithm 2. It is exactly the same logic.
+        for i in range(self.n_T, 0, -1):
+            z = torch.randn(n_sample, *size).to(device) if i > 1 else 0
+            eps = self.eps_model(
+                x_i, torch.tensor(i / self.n_T).to(device).repeat(n_sample, 1)
+            )
+            x_i = (
+                self.oneover_sqrta[i] * (x_i - eps * self.mab_over_sqrtmab[i])
+                + self.sqrt_beta_t[i] * z
+            )
+
+        return x_i
+
+# ------------------------------------------ Older version from HF tutorial ------------------------------------------
 def extract(a, t, x_shape):
     batch_size = t.shape[0]
     out = a.gather(-1, t.cpu())

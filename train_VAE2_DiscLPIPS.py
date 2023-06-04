473e61034cffafec163dfbad0e1f14460c175033
SF <stanislav.filippov@gmail.com>
2023-06-03 20:37:27 -0400

    Some changes

--- train_VAE2_DiscLPIPS.py
+++ train_VAE2_DiscLPIPS.py
@@ -102,13 +102,21 @@ def main(config_file):
         fp16 = None
 
     # device
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if torch.cuda.device_count() >= 2:
+        device_model = "cuda:0" if torch.cuda.is_available() else "cpu"
+        device_loss = "cuda:1" if torch.cuda.is_available() else "cpu"
+    else:
+        device_model = device_loss = "cuda" if torch.cuda.is_available() else "cpu"
+
+    print("Device for model? ", torch.cuda.get_device_name(device_model))
+    print("Device for loss? ", torch.cuda.get_device_name(device_loss))
 
     # init the model
-    model, model_eager = prepare_vqmodel(config, device, flag_compile)
+    model, model_eager = prepare_vqmodel(config, device_model, flag_compile)
+    encoder_tanh = config['model'].get('encode_tanh_out', False)
     # init the disc
     if config['discriminator']['enabled']:
-        discriminator = init_discriminator(config['discriminator'], device)
+        discriminator = init_discriminator(config['discriminator'], device_loss)
     else:
         discriminator = None
 
@@ -125,12 +133,12 @@ def main(config_file):
         raise ValueError
     if config['lr_scheduler']['type'].lower() == 'onecyclelr':
         config['lr_scheduler']['epochs'] = num_epochs
-        config['lr_scheduler']['steps_per_epoch'] = len(train_loader)
-    m_sch = set_lr_scheduler(config['lr_scheduler'],m_opt)
+        config['lr_scheduler']['steps_per_epoch'] = int(np.ceil(len(train_loader) / config['training']['grad_step_acc']))
+    m_sch = set_lr_scheduler(config['lr_scheduler'], m_opt)
     if discriminator:
         if config['lr_scheduler_d']['type'].lower() == 'onecyclelr':
             config['lr_scheduler_d']['epochs'] = num_epochs
-            config['lr_scheduler_d']['steps_per_epoch'] = len(train_loader)
+            config['lr_scheduler_d']['steps_per_epoch'] = int(np.ceil(len(train_loader) / config['training']['grad_step_acc']))
         d_sch = set_lr_scheduler(config['lr_scheduler_d'], d_opt)
     else:
         d_sch = None
@@ -154,13 +162,13 @@ def main(config_file):
         print(f'---------------------------------------------\nPretraining the discriminator for {d_epochs:>3} epochs\n---------------------------------------------')
         discriminator = pretrain_discriminator(discriminator, model, disc_loader, 
                                                disc_loss, d_opt, d_sch, 
-                                               d_scaler, d_epochs, device, fp16, 
+                                               d_scaler, d_epochs, device_loss, fp16,
                                                d_grad_step_acc)
         disc_name = os.path.join(chkpts, 'discriminator.pth')
         save_model(discriminator, disc_name)
         # re-init the optimizer, scaler, and scheduler
         d_opt = set_optimizer(config['optimizer_d'], discriminator, flag_compile)
-        d_sch = set_lr_scheduler(config['lr_scheduler_d'],m_opt)
+        d_sch = set_lr_scheduler(config['lr_scheduler_d'], m_opt)
         d_scaler = torch.cuda.amp.GradScaler()
         print(f'Saved pretrained discriminator in\n\t{disc_name}')
         
@@ -176,14 +184,14 @@ def main(config_file):
                                            discriminator_weight = disc_weight,
                                            disc_factor = disc_factor, 
                                            disc_loss = disc_loss_fn,
-                                           cfg = config['lpips']).to(device)
+                                           cfg = config['lpips']).to(device_loss)
 
     # ----------------------------------------------
     # Save original images
     # ----------------------------------------------
     x_original = next(iter(train_loader))
     x_original = x_original[0]
-    save_grid_imgs(unscale_tensor(x_original), max(x_original.shape[0] // 4, 2), f'{img_folder}/original-images.jpg')
+    save_grid_imgs(unscale_tensor(x_original), max(x_original.shape[0] // 4, 2), f'{results_folder}/original-images.jpg')
     
     # ----------------------------------------------
     # Training
@@ -199,8 +207,8 @@ def main(config_file):
     avg_metrics = {}
     m_loss_log = []
     d_loss_log = []
-    m_loss_fname = os.path.join(results_folder, 'model_log_loss.dat')
-    d_loss_fname = os.path.join(results_folder, 'disc_log_loss.dat')
+    m_loss_fname = os.path.join(results_folder, 'model_log_loss.csv')
+    d_loss_fname = os.path.join(results_folder, 'disc_log_loss.csv')
     for epoch in range(num_epochs):
         t_start = time.time()
         progress_bar = tqdm(train_loader, desc=f'Train {epoch+1}', total = len(train_loader),
@@ -214,13 +222,16 @@ def main(config_file):
         
         for bstep, X in enumerate(progress_bar):
             batch_size = X[0].shape[0]
-            batch = X[0].to(device, non_blocking=False)
+            batch = X[0].to(device_model, non_blocking=False)
 
             # GAN-like part
             with torch.cuda.amp.autocast(dtype = fp16):
-                batch_recon, q_loss, perplexity_s, _, _ = model(batch)
-                m_loss, m_msg = LPIPSDiscLoss(q_loss, batch, batch_recon, 0, step, last_layer = model.get_last_layer())
-            m_scaler.scale(m_loss).backward()
+                batch_recon, q_loss, perplexity_s, _, _ = model(batch, encoder_tanh)
+            with torch.cuda.amp.autocast(dtype = fp16):
+                m_loss, m_msg = LPIPSDiscLoss(q_loss.to(device_loss), batch.to(device_loss),
+                                              batch_recon.to(device_loss), 0, step,
+                                              last_layer = model.get_last_layer().to(device_loss)) # .to(device_loss)
+            m_scaler.scale(m_loss / grad_step_acc).backward()
             
             m_msg_tqdm = conv2tqdm_msg(m_msg)
             progress_bar.set_postfix(m_msg_tqdm)
@@ -232,17 +243,27 @@ def main(config_file):
                     avg_metrics[key] = 0
                 avg_metrics[key] += m_msg[key]
                 t.append(m_msg[key])
+            if m_sch:
+                t.append(m_sch.get_last_lr()[0])
             m_loss_log.append(t)
 
                 
             # Update the discriminator
             with torch.cuda.amp.autocast(dtype = fp16):
-                batch_recon, q_loss, perplexity_s, _, _ = model(batch)
-                d_loss, d_msg = LPIPSDiscLoss(q_loss, batch, batch_recon, 1, step, last_layer = model.get_last_layer())
-            d_scaler.scale(d_loss).backward()
+                batch_recon, q_loss, perplexity_s, _, _ = model(batch, encoder_tanh)
+                d_loss, d_msg = LPIPSDiscLoss(q_loss.to(device_loss), batch.to(device_loss),
+                                              batch_recon.to(device_loss), 1, step,
+                                              last_layer = model.get_last_layer().to(device_loss))
+            d_scaler.scale(d_loss / grad_step_acc).backward()
             t = []
             for key in d_msg:
                 t.append(d_msg[key])
+            else:
+                t.append(config['optimizer']['lr'])
+            if d_sch:
+                t.append(d_sch.get_last_lr()[0])
+            else:
+                t.append(config['optimizer_d']['lr'])
             d_loss_log.append(t)
 
 
@@ -250,20 +271,32 @@ def main(config_file):
             if step != 0 and step % grad_step_acc == 0  or (bstep+1) == len(train_loader):
                 m_scaler.step(m_opt)
                 m_scaler.update()
+                if config['lr_scheduler']['type'].lower() == 'onecyclelr':
+                    if m_sch:
+                        try:
+                            m_sch.step() 
+                        except Exception as e:
+                            print(e)
                 m_opt.zero_grad(set_to_none = True)
 
                 d_scaler.step(d_opt)
                 d_scaler.update()
+                if config['lr_scheduler_d']['type'].lower() == 'onecyclelr':
+                    if d_sch:
+                        try:
+                            d_sch.step() 
+                        except Exception as e:
+                            print(e)
                 d_opt.zero_grad(set_to_none = True)
-
+                
             total_loss.append(m_loss.item())    
             avg_tot += total_loss[-1]           
 
             # save generated images
             if step != 0 and (step+1) % sample_every == 0:
                 with torch.no_grad():
-                    x_recon, _, _, _, _ = model(x_original.to(device))
-                    enc_x = model.encode(x_original.to(device))
+                    x_recon, _, _, _, _ = model(x_original.to(device_model))
+                    #enc_x = model.encode(x_original.to(device_model))
 
                 x_recon = unscale_tensor(x_recon)
                 save_grid_imgs(x_recon, max(x_recon.shape[0] // 4, 2), \
@@ -271,24 +304,27 @@ def main(config_file):
                 
             # save checkpoints
             if step != 0 and (step+1) % save_every == 0:
-                checkpoint = {
-                    'model_orig': model_eager.state_dict(),
-                    'm_opt': m_opt.state_dict(),
-                    'm_scaler': m_scaler.state_dict(),
-                    'd_opt': d_opt.state_dict(),
-                    'd_scaler': d_scaler.state_dict()
-                }
-                torch.save(checkpoint, f'{chkpts}/chkpt_{step+1}-{epoch+1}.pt')
+                #checkpoint = {
+                #    'model_orig': model_eager.state_dict(),
+                #    'm_opt': m_opt.state_dict(),
+                #    'm_scaler': m_scaler.state_dict(),
+                #    'd_opt': d_opt.state_dict(),
+                #    'd_scaler': d_scaler.state_dict()
+                #}
+                #torch.save(checkpoint, f'{chkpts}/chkpt_{step+1}-{epoch+1}.pt')
                 torch.save(model_eager.state_dict(), f'{chkpts}/model_orig_{step+1}-{epoch+1}.pt')
+                # save the discriminator
+                save_model(discriminator, disc_name)
+            
             # Update the global step
             step += 1
             
             # save the losses
             t = np.array(m_loss_log)
-            np.savetxt(m_loss_fname, t, header =','.join(list(m_msg.keys())) )
+            np.savetxt(m_loss_fname, t, header =','.join(list(m_msg.keys()) + ['lr']), delimiter="," )
             t = np.array(d_loss_log)
-            np.savetxt(d_loss_fname, t, header = ','.join(list(d_msg.keys())) )
-
+            np.savetxt(d_loss_fname, t, header = ','.join(list(d_msg.keys()) + ['lr']), delimiter="," )
+            
         # save a snapshot
         if save_snapshot:
             torch.save(model_eager.state_dict(), f'{chkpts}/model_orig_snapshot.pt')
@@ -296,11 +332,13 @@ def main(config_file):
         # save the discriminator
         save_model(discriminator, disc_name)
 
-        # update scheduler
-        if m_sch:
-            m_sch.step() 
-        if d_sch:
-            d_sch.step() 
+        # update scheduler (MultiStepLR)
+        if config['lr_scheduler']['type'].lower() == 'multisteplr':
+            if m_sch:
+                m_sch.step() 
+        if config['lr_scheduler_d']['type'].lower() == 'multisteplr':
+            if d_sch:
+                d_sch.step() 
             
         avg_tot /= (bstep+1)
         #msg = f'\t----> loss: {avg_tot:<2.5f}'
